ba00d51bd7ffc78c42019907da43d614c60f8075
BertranMark <37067020+BertranMark@users.noreply.github.com>
2018-10-01 21:46:32 +0300

    Initial Commit

---
+++ AppLifeCycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppLifeCycle
+//
+//  Created by Bertran on 01.10.2018.
+//  Copyright © 2018 Bertran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
